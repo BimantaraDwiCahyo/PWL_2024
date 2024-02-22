3ed576aea506372bea45828827503ba429f36744
BimantaraDwiCahyo <27tapai@gmail.com>
2024-02-22 20:07:42 +0700

    Routing

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,43 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+ */
+Route::get('/hello', function () {
+    return 'Hello World';
 });
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/welcome', function () {
+    return 'selamat datang';
+});
+
+Route::get('/about', function () {
+    return '2241720188 <br>  Bimantara';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function
+    ($postId, $commentId) {
+        return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+    });
+
+Route::get('/articles/{id}', function ($id) {
+    return "Halaman Artikel dengan ID {{$id}}";
+});
+
+Route::get('/user/{name?}', function ($name = null) {return 'Nama saya ' . $name;
+});
+
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/user/profile', function () {
+    //
+})->name('profile');
